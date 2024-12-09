0923527a60b3de9454470066f005a9aa130dcc0d
kk-rathod <168457651+kk-rathod@users.noreply.github.com>
2024-12-09 16:11:14 +0530

    first try

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(const MyApp());
+}
 
-  class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-      return const Placeholder();
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Demo Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Home Page'),
+    );
   }
+}
 
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
 }
